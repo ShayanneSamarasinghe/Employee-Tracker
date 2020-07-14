80f48fd743cf938b3610e71bf22559faf6caf46d
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-14 09:55:18 -0400

    Schema file added

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE cms;
+USE cms;
+
+CREATE TABLE department(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30)
+);
+
+CREATE TABLE role(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INTEGER AUTO_INCREMENT 
+);
+
+CREATE TABLE employee(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30),
+    last_name DECIMAL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
+
+INSERT INTO department VALUES (1, "Web Dev");
+
+INSERT INTO role VALUES (1, "Developer", 60000.00, 1);
+
+INSERT INTO employee VALUES(1,Shayanne, Samarasinghe, 1, 1);
+INSERT INTO employee VALUES(1,Mick, Johnson, 1, 1);
+INSERT INTO employee VALUES(1,Alex, Mastro, 1, 1)
+
+
