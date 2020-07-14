ed04d9daf2c807a9f8a06832069c76686898da95
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-14 16:53:48 -0400

    added db connections to serverjs

---
+++ server.js
@@ -0,0 +1,40 @@
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+var cTable = require('console.table');
+
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+class Database {
+  constructor( config ) {
+      this.connection = mysql.createConnection( config );
+  }
+  query( sql, args ) {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.query( sql, args, ( err, rows ) => {
+              if ( err )
+                  return reject( err );
+              resolve( rows );
+          } );
+      } );
+  }
+  close() {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.end( err => {
+              if ( err )
+                  return reject( err );
+              resolve();
+          } );
+      } );
+  }
+}
+
+const db = new Database({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  insecureAuth : true
+});
