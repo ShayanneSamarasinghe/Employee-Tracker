12f9c9acf946ab8fe71e3c3b310298e58feb0c8f
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-14 10:10:27 -0400

    Minor sql adjustments

--- schema.sql
+++ schema.sql
@@ -10,13 +10,13 @@ CREATE TABLE role(
 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
     salary DECIMAL,
-    department_id INTEGER AUTO_INCREMENT 
+    department_id INTEGER
 );
 
 CREATE TABLE employee(
 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30),
-    last_name DECIMAL,
+    last_name VARCHAR (30),
     role_id INTEGER,
     manager_id INTEGER
 );
@@ -25,8 +25,8 @@ INSERT INTO department VALUES (1, "Web Dev");
 
 INSERT INTO role VALUES (1, "Developer", 60000.00, 1);
 
-INSERT INTO employee VALUES(1,Shayanne, Samarasinghe, 1, 1);
-INSERT INTO employee VALUES(1,Mick, Johnson, 1, 1);
-INSERT INTO employee VALUES(1,Alex, Mastro, 1, 1)
+INSERT INTO employee VALUES(1,"Shayanne", "Samarasinghe", 1, 1);
+INSERT INTO employee VALUES(1,"Mick", "Johnson", 1, 1);
+INSERT INTO employee VALUES(1,"Alex", "Mastro", 1, 1)
 
 
