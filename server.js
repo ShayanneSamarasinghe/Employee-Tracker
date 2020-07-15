a9f6373f2936972765cb63a327a891c04068fea3
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-15 18:33:24 -0400

    added start if/elses to start function.

--- server.js
+++ server.js
@@ -51,48 +51,61 @@ function start(){
             }
         ]
     )
-}
-
-inquirer.prompt(
-    [
-        {
-            type: "rawlist",
-            name: "viewTables",
-            message: "Would you like to view the tables?",
-            choices: ["Departments", "Role", "Employees"]
-        }
-
-    ]
-).then(function(answers){
-    console.log(answers)
-    if(answers.viewTables === "Departments"){
-      db.query("SELECT * FROM department").then(
-          function(departmentData){
-          console.table(departmentData) 
-          db.close()
-          }
-      )
-     
-
-    }else if(answers.viewTables === "Role"){
-       db.query("SELECT * FROM role").then(
-           function(roleData){
-            console.table(roleData)
-            db.close()
-           }
-       ) 
-        
-
-    }else{
-       db.query("SELECT * FROM employee").then(
-                function(employeeData){
-                console.table(employeeData)
-                db.close()
-                }
-            )
-        
-        
-
-
+}.then(function(answers){
+    if(answers.start === "view"){
+        view();
+    }
+    else if(answers.start === "add"){
+        add ();
+    }
+    else{
+        update();
     }
 })
+
+function view(){
+    inquirer.prompt(
+        [
+            {
+                type: "rawlist",
+                name: "viewTables",
+                message: "Would you like to view the tables?",
+                choices: ["Departments", "Role", "Employees"]
+            }
+    
+        ]
+    ).then(function(answers){
+        console.log(answers)
+        if(answers.viewTables === "Departments"){
+          db.query("SELECT * FROM department").then(
+              function(departmentData){
+              console.table(departmentData) 
+              db.close()
+              }
+          )
+         
+    
+        }else if(answers.viewTables === "Role"){
+           db.query("SELECT * FROM role").then(
+               function(roleData){
+                console.table(roleData)
+                db.close()
+               }
+           ) 
+            
+    
+        }else{
+           db.query("SELECT * FROM employee").then(
+                    function(employeeData){
+                    console.table(employeeData)
+                    db.close()
+                    }
+                )
+            
+            
+    
+    
+        }
+    })
+
+}
